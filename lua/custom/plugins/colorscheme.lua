eef757d8f8e29c0160f7a3df6ca2e620ea07cf65
thienduyy <nguyenvthienduy@gmail.com>
2024-06-07 15:42:59 +0700

    change: theme, shortcut TAB

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,30 +1,8 @@
 return {
-  'folke/tokyonight.nvim',
+  'doums/darcula',
   priority = 1000,
   config = function()
-    local transparent = false -- set to true if you would like to enable transparency
-
-    vim.cmd.colorscheme 'tokyonight-night'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-
-    require('tokyonight').setup {
-      style = 'night',
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and 'transparent' or 'dark',
-        floats = transparent and 'transparent' or 'dark',
-      },
-      on_colors = function(colors)
-        colors.bg_dark = transparent and colors.none or bg_dark
-        colors.bg_float = transparent and colors.none or bg_dark
-        colors.bg_sidebar = transparent and colors.none or bg_dark
-        colors.bg_statusline = transparent and colors.none or bg_dark
-      end,
-    }
-
-    vim.cmd 'colorscheme tokyonight'
+    vim.cmd 'colorscheme darcula'
   end,
 }
 
